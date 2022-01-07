1535fc33c925691b39a5b1cfc66f9a495900959d
Jueun <jueunsuh@gmail.com>
2022-01-07 14:56:32 -0600

    Implement all sections: video (for all youtube url)

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -16,7 +16,7 @@ export class VideoComponent extends BaseComponent<HTMLLIElement> {
 
   // https://www.youtube.com/watch?v=HM6UpQZvbhY
   // https://youtu.be/HM6UpQZvbhY
-  // 정규표현식 Regex
+  // Regex
   private convertToEmbeddedURL(url:string): string {
     const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
     const match = url.match(regExp);
