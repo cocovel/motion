88f852a44396dfc06f848f5a7ec56e46f38821c8
Jueun <jueunsuh@gmail.com>
2022-02-02 15:05:23 -0600

    Dependency Injection Refactoring

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,7 +4,16 @@ export interface Composable {
   addChild(child: Component): void;
 }
 type OnCloseListener = () => void;
-class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+
+interface SectionContainer extends Component, Composable {
+  setOnCloseListener(listener: OnCloseListener): void;
+}
+
+type SectionContainerConstructor = {
+  new (): SectionContainer;
+}
+
+export class PageItemComponent extends BaseComponent<HTMLElement> implements SectionContainer {
   private closeListener?: OnCloseListener;
 
   constructor() {
@@ -13,7 +22,7 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
             <div class="page-item__controls">
               <button class="close">&times;</button>
             </div>
-          </li>`);
+          </li>`);  
     const closeBtn = this.element.querySelector('.close') as HTMLButtonElement;
     closeBtn.onclick = () => {
       this.closeListener && this.closeListener();
@@ -30,12 +39,12 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
 }
 
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
-  constructor() {
+  constructor(private pageItemConstructor: SectionContainerConstructor) {
     super('<ul class="page"></ul>');
   }
-
+ 
   addChild(section: Component) {
-    const item = new PageItemComponent();
+    const item = new this.pageItemConstructor();
     item.addChild(section);
     item.attachTo(this.element, 'beforeend');
     item.setOnCloseListener(() => {
